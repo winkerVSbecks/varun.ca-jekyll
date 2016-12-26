e6fee06d6cc514af25e3a208091a3d39c4de0cab
Varun Vachhar <varunvachhar@gmail.com>
2016-12-26 16:54:10 -0500

    📝

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   use: [
     'postcss-import',
-    'autoprefixer',
     'cssnano',
   ],
   input: '_css/index.css',
   output: '_includes/index.css',
   'local-plugins': true,
+  cssnano: {
+    colormin: false,
+  }
 };
 
 // const headingQuery = 'h2[id],h3[id],h4[id],h5[id],h6[id]';
