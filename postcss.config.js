988701d7bb9c3f8365e8bfeef2ca6a81c45e0109
Varun Vachhar <varunvachhar@gmail.com>
2016-12-27 17:45:23 -0500

    📝

--- postcss.config.js
+++ postcss.config.js
@@ -10,14 +10,3 @@ module.exports = {
     colormin: false,
   }
 };
-
-// const headingQuery = 'h2[id],h3[id],h4[id],h5[id],h6[id]';
-// const headings = document.querySelectorAll(headingQuery);
-// [ ...headings].forEach(function(heading) {
-//   heading.innerHTML = `
-//     <a href="#${ heading.id }"
-//       class="link"
-//       style="color: inherit;">
-//       ${ heading.innerText }
-//     </a>`;
-// });
